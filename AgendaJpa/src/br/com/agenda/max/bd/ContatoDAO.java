0b3a4c44bfc8fae4092328dc8e0800910cf61cf1
Max Denner <maxdenner027@hotmail.com>
2019-05-30 16:57:17 -0300

    Versão 1.1
    Alterações:
    	ControllerMVC implementado;
    	Interface Logica implementada;
    	EclipseLink configurado;
    	Módulo de cadastro de contatos implementado;
    	Módolo de listagem de condados.
    
    
    
    Signed-off-by: Max Denner <maxdenner027@hotmail.com>

--- AgendaJpa/src/br/com/agenda/max/bd/ContatoDAO.java
+++ AgendaJpa/src/br/com/agenda/max/bd/ContatoDAO.java
@@ -1,24 +1,37 @@
 package br.com.agenda.max.bd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import br.com.agenda.max.modelo.Contato;
 
 public class ContatoDAO {
+	private EntityManagerFactory fabrica;
+	private EntityManager manager;
 
 	public ContatoDAO() {
-		
+		fabrica = Persistence.createEntityManagerFactory("agenda-pu");
+		manager = fabrica.createEntityManager();
 	}
 
 	public void adicionaContato(Contato contato) {
-		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("agenda-pu");
-		EntityManager manager = fabrica.createEntityManager();
 		manager.persist(contato);
 		manager.getTransaction().begin();
 		manager.getTransaction().commit();
 		manager.close();
 		fabrica.close();
+	}
+
+	public List<Contato> listaContatos() {
+		Query query = manager.createQuery("Select c from contatos c");
+		List<Contato> contatos = query.getResultList();
+		manager.close();
+		fabrica.close();
+		return contatos;
 	}
 }
